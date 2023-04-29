2ffe4e5b9e0ce9c56fd55b4b98fb150703c327e5
Jianan Wang <100524861+unset996@users.noreply.github.com>
2023-04-29 01:56:56 +0100

    Update option.sol

--- back-end/option.sol
+++ back-end/option.sol
@@ -1,3 +1,5 @@
+/*
+
 // Solidity version
 pragma solidity ^0.8.0;
 
@@ -178,4 +180,214 @@ contract LiquidityMining {
         return address(this// Return contract owner
         ); 
     }
+}
+
+*/
+
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+// Import Chainlink related contracts
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FuturesAndOptionsContract {
+    address public buyer; // address of the buyer
+    address public seller; // address of the seller
+    uint256 public price; // price of the futures contract
+    uint256 public strikePrice; // strike price of the options contract
+    uint256 public quantity; // quantity of the underlying asset
+    uint256 public expiration; // expiration date of the contract
+    uint256 public margin; // margin required to open a position
+    uint256 public leverage; // leverage ratio of the position
+    uint256 public stopLoss; // price at which a stop-loss order is triggered
+    uint256 public marginCall; // margin level at which a margin call is triggered
+    
+    // Chainlink oracle-related variable
+    AggregatorV3Interface internal priceFeed;
+   
+    // Mapping to store trade records for each trader
+    mapping(address => Trade[]) public trades;
+    
+    // Struct to store trade records
+    struct Trade {
+        uint256 timestamp;
+        uint256 quantity;
+        uint256 price;
+        bool isBuyOrder;
+        bool isOption; // flag to indicate if the trade is an option
+    }
+
+    // Define the state of the contract using an enum
+    enum ContractState {
+        Created, // the contract has been created but not yet locked
+        Locked, // the contract has been locked and is active
+        Inactive // the contract has expired, been settled or terminated
+    }
+    ContractState public state;
+
+    // Define the events of the contract
+    event ContractLocked();
+    event ContractSettled(address indexed winner, uint256 payout);
+    event MarketOrderExecuted(address indexed trader, uint256 quantity, uint256 price, bool isBuyOrder, bool isOption);
+    event StopLossOrderExecuted(address indexed trader, uint256 quantity, uint256 price, bool isBuyOrder);
+    event MarginCallTriggered(address indexed trader, uint256 marginLevel);
+
+    // Constructor to initialize the contract parameters
+    constructor(
+        address _buyer, // address of the buyer
+        address _seller, // address of the seller
+        uint256 _price, // price of the futures contract
+        uint256 _strikePrice, // strike price of the options contract
+        uint256 _quantity, // quantity of the underlying asset
+        uint256 _expiration, // expiration date of the contract
+        uint256 _margin, // margin required to open a position
+        uint256 _leverage, // leverage ratio of the position
+        uint256 _stopLoss, // price at which a stop-loss order is triggered
+        uint256 _marginCall, // margin level at which a margin call is triggered
+        address _priceFeedAddress // Chainlink oracle address parameter
+    ) {
+        buyer = _buyer;
+        seller = _seller;
+        price = _price;
+        strikePrice = _strikePrice;
+        quantity = _quantity;
+        expiration = _expiration;
+        margin = _margin;
+        leverage = _leverage;
+        stopLoss = _stopLoss;
+        marginCall = _marginCall;
+       
+        state = ContractState.Created;
+    
+    // Initialize Chainlink oracle
+    priceFeed = AggregatorV3Interface(_priceFeedAddress);
+}
+
+// Lock the contract to activate it and open a position
+function lockContract() public payable {
+    // Check that the caller is the buyer or the seller
+    require(msg.sender == buyer || msg.sender == seller, "Only buyer or seller can lock the contract");
+    // Check that the contract is in the correct state to be locked
+    require(state == ContractState.Created, "Contract is not in the correct state to be locked");
+    // Check that the margin is sufficient to open a position
+    require(msg.value >= margin, "Insufficient margin to open a position");
+    // Calculate the amount of the underlying asset that can be bought with the margin and leverage
+    uint256 positionSize = (msg.value * leverage * price) / 1 ether;
+    // Change the state of the contract to Locked
+    state = ContractState.Locked;
+    // Emit the ContractLocked event
+    emit ContractLocked();
+}
+
+// Execute a market order to buy or sell the underlying asset
+function executeMarketOrder(bool isBuyOrder, uint256 orderQuantity, bool isOption) public payable {
+    // Check that the caller is either the buyer or the seller
+    require(msg.sender == buyer || msg.sender == seller, "Only buyer or seller can execute a market order");
+    // Check that the contract is in the correct state to execute a market order
+    require(state == ContractState.Locked, "Contract is not in the correct state to execute a market order");
+    // Calculate the cost of the order based on the price and quantity of the contract
+    
+    uint256 currentPrice = getPrice();
+    uint256 cost = price * quantity;
+    
+    // Check that the trader has sufficient funds or assets to execute the order
+    if (isBuyOrder) {
+        require(msg.sender == buyer, "Only the buyer can execute a buy order");
+        require(msg.value >= cost, "Insufficient funds to execute the buy order");
+        // Calculate the new position size after executing the order
+        uint256 newPositionSize = ((msg.value - cost) * leverage * price) / 1 ether;
+        // Check if a margin call has been triggered
+        if (newPositionSize * price < marginCall * margin) {
+            // Trigger a margin call if the new position size falls below the margin call level
+            emit MarginCallTriggered(msg.sender, newPositionSize * price / margin);
+        }
+    } else {
+        require(msg.sender == seller, "Only the seller can execute a sell order");
+        require(quantity <= quantity - (msg.value / price), "Insufficient assets to execute the sell order");
+        // Calculate the new position size after executing the order
+        uint256 newPositionSize = ((msg.value + cost) * leverage * price) / 1 ether;
+        // Check if a margin call has been triggered
+        if (newPositionSize * price < marginCall * margin) {
+            // Trigger a margin call if the new position size falls below the margin call level
+            emit MarginCallTriggered(msg.sender, newPositionSize * price / margin);
+        }
+    }
+    // Add the executed trade to the trade history and emit the MarketOrderExecuted event
+    Trade memory newTrade = Trade({
+        timestamp: block.timestamp,
+        quantity: orderQuantity,
+        price: currentPrice,
+        isBuyOrder: isBuyOrder,
+        isOption: isOption // set the isOption flag
+    });
+
+    trades[msg.sender].push(newTrade);
+
+    emit MarketOrderExecuted(msg.sender, orderQuantity, currentPrice, isBuyOrder, isOption);
+    
+    }
+    
+// Settle the contract when it expires, or when a stop-loss order or margin call is triggered
+function settleContract() public {
+    // Check that the contract is in the correct state to be settled
+    require(state == ContractState.Locked, "Contract is not in the correct state to be settled");
+
+    // Check if the contract has expired, or if a stop-loss order or margin call has been triggered
+    if (block.timestamp >= expiration || getPrice() <= stopLoss) {
+        // Change the state of the contract to Inactive
+        state = ContractState.Inactive;
+
+        uint256 payout;
+        address winner;
+
+        // Determine the winner and payout based on the last trade
+        Trade memory lastTrade = trades[msg.sender][trades[msg.sender].length - 1];
+
+        if (lastTrade.isBuyOrder) {
+            // If the last trade was a buy order and the current price is higher than the strike price, the buyer wins
+            if (getPrice() > strikePrice && lastTrade.isOption) {
+                winner = buyer;
+                payout = (getPrice() - strikePrice) * lastTrade.quantity;
+            } else {
+                // If the current price is lower than the strike price, the seller wins
+                winner = seller;
+                payout = (strikePrice - getPrice()) * lastTrade.quantity;
+            }
+        } else {
+            // If the last trade was a sell order and the current price is lower than the strike price, the seller wins
+            if (getPrice() < strikePrice) {
+                winner = seller;
+                payout = (strikePrice - getPrice()) * lastTrade.quantity;
+            } else {
+                // If the current price is higher than the strike price, the buyer wins
+                winner = buyer;
+                payout = (getPrice() - strikePrice) * lastTrade.quantity;
+            }
+        }
+
+        // Transfer the payout to the winner
+        (bool success, ) = winner.call{value: payout}("");
+        require(success, "Transfer to winner failed");
+
+        // Emit the ContractSettled event
+        emit ContractSettled(winner, payout);
+    }
+}
+
+// Get the latest price of the underlying asset using Chainlink oracle
+function getPrice() public view returns (uint256) {
+    (
+        uint80 roundID,
+        int256 price,
+        uint256 startedAt,
+        uint256 timeStamp,
+        uint80 answeredInRound
+    ) = priceFeed.latestRoundData();
+    return uint256(price);
+}
+
+// Helper function to get the number of trades executed by a trader
+function getTradeCount(address trader) public view returns (uint256) {
+    return trades[trader].length;
 }
