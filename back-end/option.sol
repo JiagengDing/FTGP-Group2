e8778d8db92c30b321a2a9d1da76533f8ea9c761
Whisper_xx <115017454+LongxiHe@users.noreply.github.com>
2023-05-15 17:58:41 +0800

    Update option.sol

--- back-end/option.sol
+++ back-end/option.sol
@@ -1,25 +1,27 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Option {
     struct CallOption {
-        uint256 expiration;
-        uint256 strikePrice;
-        address underlying;
-        address seller;
-        address buyer;
-        bool exercised;
+        uint256 expiration;     // Option's expiration time
+        uint256 strikePrice;    // The price at which the option can be exercised
+        address underlying;    // Address of the underlying asset
+        address seller;        // Address of the seller
+        address buyer;         // Address of the buyer
+        bool exercised;        // Whether the option has been exercised
     }
 
-    mapping(uint256 => CallOption) public options;
-    uint256 public nextOptionId;
+    mapping(uint256 => CallOption) public options;  // Stores all the options
+    uint256 public nextOptionId;                    // ID for the next option
 
+    // Creates a new option
     function createOption(
-        uint256 _expiration,
-        uint256 _strikePrice,
-        address _underlying,
-        address _buyer
+        uint256 _expiration,     // Expiration time
+        uint256 _strikePrice,    // Strike price
+        address _underlying,    // Address of the underlying asset
+        address _buyer          // Address of the buyer
     ) external {
         options[nextOptionId] = CallOption({
             expiration: _expiration,
@@ -32,6 +34,7 @@ contract Option {
         nextOptionId++;
     }
 
+    // Buyer exercises the option and pays the strike price to the seller
     function buyOption(uint256 _optionId) external payable {
         CallOption storage option = options[_optionId];
         require(msg.sender == option.buyer, "Only the buyer can buy the option");
@@ -43,6 +46,7 @@ contract Option {
         payable(option.seller).transfer(msg.value);
     }
 
+    // Seller sells the option to a new buyer
     function sellOption(uint256 _optionId, address _newBuyer) external {
         CallOption storage option = options[_optionId];
         require(msg.sender == option.seller, "Only the seller can sell the option");
@@ -51,6 +55,7 @@ contract Option {
         option.buyer = _newBuyer;
     }
 
+    // Buyer buys the option with tokens and the tokens are transferred from buyer to the seller
     function buyTokenOption(uint256 _optionId, uint256 _amount) external {
         CallOption storage option = options[_optionId];
         require(msg.sender == option.buyer, "Only the buyer can buy the option");
@@ -62,6 +67,7 @@ contract Option {
         option.exercised = true;
     }
 
+    // Cancels an option, only the seller or buyer can cancel
     function cancelOption(uint256 _optionId) external {
         CallOption storage option = options[_optionId];
         require(
@@ -73,6 +79,7 @@ contract Option {
         delete options[_optionId];
     }
 
+    // Changes the strike price of an option
     function changeStrikePrice(uint256 _optionId, uint256 _newStrikePrice)
         external
     {
@@ -83,6 +90,7 @@ contract Option {
         option.strikePrice = _newStrikePrice;
     }
 
+    // Changes the expiration time of an option
     function changeExpiration(uint256 _optionId, uint256 _newExpiration)
         external
     {
@@ -93,6 +101,7 @@ contract Option {
         option.expiration = _newExpiration;
     }
 
+    // Changes the underlying asset of an option
     function changeUnderlying(uint256 _optionId, address _newUnderlying)
         external
     {
