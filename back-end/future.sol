bcb34d27e6bcb4ba8227df429cd967de830ec3e1
uuTing <114074338+uuTing@users.noreply.github.com>
2023-05-18 18:56:59 +0800

    Update future.sol

--- back-end/future.sol
+++ back-end/future.sol
@@ -1,169 +1,96 @@
 pragma solidity ^0.8.0;
 
 contract FuturesContract {
-    address public buyer; // address of the buyer
-    address public seller; // address of the seller
-    uint public price; // price of the futures contract
-    uint public quantity; // quantity of the underlying asset
-    uint public expiration; // expiration date of the contract
-    uint public margin; // margin required to open a position
-    uint public leverage; // leverage ratio of the position
-    uint public stopLoss; // price at which a stop-loss order is triggered
-    uint public marginCall; // margin level at which a margin call is triggered
-
-    // Define the state of the contract using an enum
-    enum ContractState {
-        Created, // the contract has been created but not yet locked
-        Locked, // the contract has been locked and is active
-        Inactive // the contract has expired, been settled or terminated
-    }
-    ContractState public state;
-
-    // Define the events of the contract
-    event ContractLocked();
-    event ContractSettled(address indexed winner, uint payout);
-    event MarketOrderExecuted(address indexed trader, uint quantity, uint price);
-    event StopLossOrderExecuted(address indexed trader, uint quantity, uint price);
-    event MarginCallTriggered(address indexed trader, uint marginLevel);
-
-    // Define the constructor to initialize the contract parameters
-    constructor(
-        address _buyer, // address of the buyer
-        address _seller, // address of the seller
-        uint _price, // price of the futures contract
-        uint _quantity, // quantity of the underlying asset
-        uint _expiration, // expiration date of the contract
-        uint _margin, // margin required to open a position
-        uint _leverage, // leverage ratio of the position
-        uint _stopLoss, // price at which a stop-loss order is triggered
-        uint _marginCall // margin level at which a margin call is triggered
-    ) {
-        buyer = _buyer;
-        seller = _seller;
-        price = _price;
-        quantity = _quantity;
-        expiration = _expiration;
-        margin = _margin;
-        leverage = _leverage;
-        stopLoss = _stopLoss;
-        marginCall = _marginCall;
-        state = ContractState.Created;
+    // Contract owner
+    address public owner;
+    // Service fee percentage
+    uint256 public serviceFee;
+    // User balances
+    mapping(address => uint256) public balances;
+    // Underlying asset prices
+    mapping(string => uint256) public prices;
+    // Trade records
+    mapping(address => Trade[]) public trades;
+    // Trade struct
+    struct Trade {
+        address buyer;
+        address seller;
+        string asset;
+        uint256 price;
+        uint256 quantity;
+        uint256 timestamp;
     }
 
-    // Lock the contract to activate it and open a position
-    function lockContract() public payable {
-        // Check that the caller is the buyer or the seller
-        require(msg.sender == buyer || msg.sender == seller, "Only buyer or seller can lock the contract");
-        // Check that the contract is in the correct state to be locked
-        require(state == ContractState.Created, "Contract is not in the correct state to be locked");
-        // Check that the margin is sufficient to open a position
-        require(msg.value >= margin, "Insufficient margin to open a position");
-        // Calculate the amount of the underlying asset that can be bought with the margin and leverage
-        uint positionSize = (msg.value * leverage * price) / 1 ether;
-        // Change the state of the contract to Locked
-        state = ContractState.Locked;
-        // Emit the ContractLocked event
-        emit ContractLocked();
+    // Trade event
+    event TradeEvent(
+        address indexed buyer,
+        address indexed seller,
+        string asset,
+        uint256 price,
+        uint256 quantity
+    );
+
+    // Constructor to set the service fee percentage
+    constructor(uint256 _serviceFee) {
+        owner = msg.sender;
+        serviceFee = _serviceFee;
     }
 
-    // Execute a market order to buy or sell the underlying asset
-    function executeMarketOrder(bool isBuyOrder, uint quantity) public payable {
-        // Check that the caller is either the buyer or the seller
-        require(msg.sender == buyer || msg.sender == seller, "Only buyer or seller can execute a market order");
-        // Check that the contract is in the correct state to execute a market order
-        require(state == ContractState.Locked, "Contract is not in the correct state to execute a market order");
-        // Calculate the cost of the order based on the price and quantity of the contract
-        uint cost = price * quantity;
-        // Check that the trader has sufficient funds or assets to execute the order
-        if (isBuyOrder) {
-            require(msg.sender == buyer, "Only the buyer can execute a buy order");
-            require(msg.value >= cost, "Insufficient funds to execute the buy order");
-            // Calculate the new position size after executing the order
-            uint newPositionSize = ((msg.value - cost) * leverage * price) / 1 ether;
-            // Check if a margin call has been triggered
-            if (newPositionSize * price < marginCall * margin) {
-                // Trigger a margin call if the new position size falls below the margin call level
-                emit MarginCallTriggered(msg.sender, newPositionSize * price / margin);
-            }
-        } else {
-            require(msg.sender == seller, "Only the seller can execute a sell order");
-            require(quantity <= quantity - (msg.value / price), "Insufficient assets to execute the sell order");
-            // Calculate the new position size after executing the order
-            uint newPositionSize = ((msg.value + cost) * leverage * price) / 1 ether;
-            // Check if a margin call has been triggered
-            if (newPositionSize * price < marginCall * margin) {
-                // Trigger a margin call if the new position size falls below the margin call level
-                emit MarginCallTriggered(msg.sender, newPositionSize * price / margin);
-            }
-        }
-        // Emit the MarketOrderExecuted event with the trader, quantity, and price as parameters
-        emit MarketOrderExecuted(msg.sender, quantity, price);
+    // Set the price of an underlying asset, only the contract owner can call this function
+    function setPrice(string memory asset, uint256 price) public {
+        require(msg.sender == owner, "Only owner can set price");
+        prices[asset] = price;
     }
 
-    // Execute a stop-loss order to limit potential losses
-    function executeStopLossOrder(bool isBuyOrder, uint quantity) public payable{
-        // Check that the caller is either the buyer or the seller
-        require(msg.sender == buyer || msg.sender == seller, "Only buyer or seller can execute a stop-loss order");
-        // Check that the contract is in the correct state to execute a stop-loss order
-        require(state == ContractState.Locked, "Contract is not in the correct state to execute a stop-loss order");
-        // Calculate the cost of the order based on the price and quantity of the contract
-        uint cost = price * quantity;
-        // Check that the trader has not exceeded the stop-loss price
-        if (isBuyOrder) {
-            require(price <= stopLoss, "Stop-loss price not reached for buy order");
-            // Calculate the new position size after executing the order
-            uint newPositionSize = ((msg.value - cost) * leverage * price) / 1 ether;
-            // Check if a margin call has been triggered
-            if (newPositionSize * price < marginCall * margin) {
-                // Trigger a margin call if the new position size falls below the margin call level
-                emit MarginCallTriggered(msg.sender, newPositionSize * price / margin);
-            }
-        } else {
-            require(price >= stopLoss, "Stop-loss price not reached for sell order");
-            // Calculate the new position size after executing the order
-            uint newPositionSize = ((msg.value + cost) * leverage * price) / 1 ether;
-            // Check if a margin call has been triggered
-            if (newPositionSize * price < marginCall * margin) {
-                // Trigger a margin call if the new position size falls below the margin call level
-                emit MarginCallTriggered(msg.sender, newPositionSize * price / margin);
-            }
-        }
-        // Emit the StopLossOrderExecuted event with the trader, quantity, and price as parameters
-        emit StopLossOrderExecuted(msg.sender, quantity, price);
+    // Get the price of an underlying asset
+    function getPrice(string memory asset) public view returns (uint256) {
+        return prices[asset];
     }
 
-    // Settle the contract by determining the winner and transferring the payout
-    function settleContract() public {
-        // Check that the contract is in the correct state to be settled
-        require(state == ContractState.Locked, "Contract is not in the correct state to be settled");
-        // Calculate the payout based on the difference between the current price and the contract price
-        uint payout = ((price - getPrice()) * quantity * leverage) / 1 ether;
-        // Determine the winner based on the price of the underlying asset
-        address winner = payout > 0 ? buyer : seller;
-        // Transfer the payout to the winner
-        payable(winner).transfer(payout);
-        // Change the state of the contract to Inactive
-        state = ContractState.Inactive;
-        // Emit the ContractSettled event with the winner and payout as parameters
-        emit ContractSettled(winner, payout);
+    // Execute a trade between a buyer and a seller and collect a service fee from both parties
+    function trade(
+        address buyer,
+        address seller,
+        string memory asset,
+        uint256 quantity
+    ) public {
+        uint256 price = prices[asset];
+        require(price > 0, "Asset not found");
+        uint256 totalCost = price * quantity;
+        require(balances[buyer] >= totalCost, "Insufficient balance");
+        balances[buyer] -= totalCost;
+        uint256 fee = (totalCost * serviceFee) / 100;
+        balances[owner] += fee;
+        balances[seller] += totalCost - fee;
+
+        Trade memory trade = Trade(
+            buyer,
+            seller,
+            asset,
+            price,
+            quantity,
+            block.timestamp
+        );
+        trades[buyer].push(trade);
+        trades[seller].push(trade);
+
+        emit TradeEvent(buyer, seller, asset, price, quantity);
     }
 
-    // Get the current price of the underlying asset
-    function getPrice() public view returns (uint) {
-        // This function would query an oracle or other external data source to get the current price of the asset
-        // For simplicity, we're just returning a random number between 1 and 1000
-        return uint(keccak256(abi.encodePacked(block.timestamp))) % 1000 + 1;
+    // Get a user's trade records
+    function getTrades(address user) public view returns (Trade[] memory) {
+        return trades[user];
     }
 
-    // Terminate the contract and return the margin to the buyer
-    function terminateContract() public {
-        // Check that the contract is in the correct state to be terminated
-        require(state == ContractState.Created || state == ContractState.Locked, "Contract is not in the correct state to be terminated");
-        // Check that the caller is the buyer
-        require(msg.sender == buyer, "Only the buyer can terminate the contract");
-        // Transfer the margin back to the buyer
-        payable(buyer).transfer(margin);
-        // Change the state of the contract to Inactive
-        state = ContractState.Inactive;
+    // Deposit funds into the contract
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    // Withdraw funds from the contract
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
     }
 }
