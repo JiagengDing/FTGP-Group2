ad1df33c79d3785c4c49417a530d5b3754b4bcfd
yl <114348247+brendalu21fav@users.noreply.github.com>
2023-05-27 04:07:49 +0100

    add factory contract for manage file, update conntract address function for trade file

--- contracts/portfolio/manage.sol
+++ contracts/portfolio/manage.sol
@@ -4,6 +4,46 @@ pragma solidity ^0.8.10;
 //import "@openzeppelin/contracts/token/ERC20/IERC20.sol";  // Import the ERC20 interface
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol"; // Import the Chainlink Oracle interface
 
+contract ECR20_PTokenFactory {
+    event PTokenCreated(address indexed tokenAddress, address indexed creator);
+
+    mapping(address => address) public createdTokens;
+
+    function createPToken(
+        string memory _name,
+        string memory _symbol,
+        //uint8 _decimals,
+        uint _totalSupply,
+        uint256 _hopeRatio, 
+        uint256 _usdcRatio,
+        uint256 _linkRatio,
+        uint256 _customIPRate
+    ) public returns (address) {
+
+    ECR20_PToken newToken = new ECR20_PToken(
+    _name,
+    _symbol,
+    //_decimals,
+    _totalSupply,
+    msg.sender,   // pass the user's address as the owner
+    _hopeRatio,
+    _usdcRatio,
+    _linkRatio,
+    _customIPRate
+);
+
+        address tokenAddress = address(newToken);
+        createdTokens[msg.sender] = tokenAddress;
+        emit PTokenCreated(tokenAddress, msg.sender);
+        return tokenAddress;
+    }
+    
+    function getTokenAddress() public view returns (address) {
+        return createdTokens[msg.sender];
+    }
+
+}
+
 interface IERC20 {
     function totalSupply() external view returns (uint);
 
@@ -23,62 +63,48 @@ contract ECR20_PToken is IERC20 {
     mapping(address => uint) public balanceOf;
     address public owner;
 
-    IERC20 public daiToken = IERC20(0x82fb927676b53b6eE07904780c7be9b4B50dB80b); // DAI address on sepolia
+    IERC20 public hopeToken = IERC20(0x82fb927676b53b6eE07904780c7be9b4B50dB80b); // HOPE address on sepolia
     IERC20 public usdcToken = IERC20(0x2B0974b96511a728CA6342597471366D3444Aa2a); // USDC address on mainnet
     IERC20 public linkToken = IERC20(0x6B904451abABB342D2b787C5126C6361dD815246); // LINK address on mainnet
 
-    uint256 public daiRatio;
+    uint256 public hopeRatio;
     uint256 public usdcRatio;
     uint256 public linkRatio;
-
-    AggregatorV3Interface public daiPriceFeed = AggregatorV3Interface(0x14866185B1962B63C3Ea9E03Bc1da838bab34C19); // DAI/USD on sepolia
+ 
+    AggregatorV3Interface public hopePriceFeed = AggregatorV3Interface(0x14866185B1962B63C3Ea9E03Bc1da838bab34C19); // HOPE/USD on sepolia
     AggregatorV3Interface public usdcPriceFeed = AggregatorV3Interface(0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E); // USDC/USD on sepolia
     AggregatorV3Interface public linkPriceFeed = AggregatorV3Interface(0xc59E3633BAAC79493d908e63626716e204A45EdF); // LINK/USD on sepolia
 
-    // address private owner;   // if you do not want to display owner's address after deploy the contract
-
     constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimals,
-        uint _totalSupply,
-        //address owner
-        uint256 daiRatio_,
-        uint256 usdcRatio_,
-        uint256 linkRatio_,
-        uint256 customIPRate
-    ) {
-        require(daiRatio_ + usdcRatio_ + linkRatio_ == 100, "Total ratio must be 100%");
+    string memory _name,
+    string memory _symbol,
+    //uint8 _decimals,
+    uint _totalSupply,
+    address _owner,   // pass the owner's address as a parameter
+    uint256 hopeRatio_, 
+    uint256 usdcRatio_, 
+    uint256 linkRatio_,
+    uint256 customIPRate
+) {
+    require(hopeRatio_ + usdcRatio_ + linkRatio_ == 100, "Total ratio must be 100%");
 
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply*(10 ** decimals);
-        balanceOf[msg.sender] = _totalSupply*(10 ** decimals);
-        emit Transfer(address(0), msg.sender, _totalSupply);
-        owner = msg.sender;
+    name = _name;
+    symbol = _symbol;
+    // decimals = _decimals;
+    decimals = 18;
+    totalSupply = _totalSupply*(10 ** decimals);
+    //balanceOf[_owner] = _totalSupply*(10 ** decimals); // assign the initial supply to _owner
+    balanceOf[_owner] = totalSupply; // assign the initial supply to _owner
+    emit Transfer(address(0), _owner, _totalSupply);
+    owner = _owner;
 
-        daiRatio = daiRatio_;
-        usdcRatio = usdcRatio_;
-        linkRatio = linkRatio_;
+    hopeRatio = hopeRatio_;
+    usdcRatio = usdcRatio_;
+    linkRatio = linkRatio_;
 
-        customIPRate = customIPRate;
-    }
-         function getLatestPrice() public view returns (uint256) {
-        (,int daiPrice,,,) = daiPriceFeed.latestRoundData();
-        (,int usdcPrice,,,) = usdcPriceFeed.latestRoundData();
-        (,int linkPrice,,,) = linkPriceFeed.latestRoundData();
+    customIPRate = customIPRate;
+}
 
-        uint256 daiPrice18Decimals = uint256(daiPrice) * 10**10;
-        uint256 usdcPrice18Decimals = uint256(usdcPrice) * 10**10;
-        uint256 linkPrice18Decimals = uint256(linkPrice) * 10**10;
-
-        uint256 totalDaiPrice = uint256(daiPrice18Decimals) * daiRatio;
-        uint256 totalUsdcPrice = uint256(usdcPrice18Decimals) * usdcRatio;
-        uint256 totalLinkPrice = uint256(linkPrice18Decimals) * linkRatio;
-
-        return (totalDaiPrice + totalUsdcPrice + totalLinkPrice) / 100;
-    }
 
     function transfer(address recipient, uint amount) external returns (bool) {
         balanceOf[msg.sender] -= amount;
@@ -105,4 +131,31 @@ contract ECR20_PToken is IERC20 {
     function getOwnerBalanceOf() external view returns (uint) {
         return balanceOf[msg.sender];
     }
+    
+    function getContractAddress() public view returns (address) {
+        return address(this);
+    }
+
+    function getLatestPrice() public view returns (uint256) {
+        (,int hopePrice,,,) = hopePriceFeed.latestRoundData();
+        (,int usdcPrice,,,) = usdcPriceFeed.latestRoundData();
+        (,int linkPrice,,,) = linkPriceFeed.latestRoundData();
+
+        uint256 hopePrice18Decimals = uint256(hopePrice) * 10**18;
+        uint256 usdcPrice18Decimals = uint256(usdcPrice) * 10**18;
+        uint256 linkPrice18Decimals = uint256(linkPrice) * 10**18;
+
+        uint256 totalHopePrice = uint256(hopePrice18Decimals) * hopeRatio;
+        uint256 totalUsdcPrice = uint256(usdcPrice18Decimals) * usdcRatio;
+        uint256 totalLinkPrice = uint256(linkPrice18Decimals) * linkRatio;
+
+        return (totalHopePrice + totalUsdcPrice + totalLinkPrice) / 100;
+    }
+
+        function getTotalSupply() public view returns (uint){
+        return totalSupply;
+    }
 }
+
+
+
--- contracts/portfolio/trade.sol
+++ contracts/portfolio/trade.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";  // Import the ERC20 interface
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol"; // Import the Chainlink Oracle interface
 
 interface IERC20 {
     function totalSupply() external view returns (uint);
@@ -10,43 +12,59 @@ interface IERC20 {
     function transfer(address recipient, uint amount) external returns (bool);
 
     event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-contract ECR20_TestToken is IERC20 {
-    string public name = "test";
-    string public symbol = "TEST";
-    uint8 public decimals = 18;
-    // uint public override totalSupply = 100;
+
+contract ECR20_PToken is IERC20 {
+    string public name;
+    string public symbol;
+    // uint8 public decimals;
+    uint8 private decimals;
     uint public override totalSupply;
     mapping(address => uint) public balanceOf;
-    address public owner;
-    uint private current_totalsupply = 100;
-    
+    address private owner;
 
-    constructor(
-        uint _buyNum1
-        
-    ) {
-        uint _buyNum;
-        _buyNum = _buyNum1 * (10**18);
-        name = name;
-        symbol = symbol;
-        decimals = decimals;
-        // totalSupply = totalSupply*(10 ** decimals);
-        totalSupply = _buyNum + current_totalsupply *(10 ** decimals);
-        balanceOf[msg.sender] = _buyNum;
-        // emit Transfer(address(0), msg.sender, totalSupply);
-        emit Transfer(address(0), msg.sender, balanceOf[msg.sender]);
-        owner = msg.sender;
+
+    IERC20 private hopeToken = IERC20(0x82fb927676b53b6eE07904780c7be9b4B50dB80b); // HOPE address on sepolia
+    IERC20 private usdcToken = IERC20(0x2B0974b96511a728CA6342597471366D3444Aa2a); // USDC address on mainnet
+    IERC20 private linkToken = IERC20(0x6B904451abABB342D2b787C5126C6361dD815246); // LINK address on mainnet
+
+
+
+    uint256 public hopeRatio;
+    uint256 public usdcRatio;
+    uint256 public linkRatio;
+ 
+    AggregatorV3Interface private hopePriceFeed = AggregatorV3Interface(0x14866185B1962B63C3Ea9E03Bc1da838bab34C19); // HOPE/USD on sepolia
+    AggregatorV3Interface private usdcPriceFeed = AggregatorV3Interface(0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E); // USDC/USD on sepolia
+    AggregatorV3Interface private linkPriceFeed = AggregatorV3Interface(0xc59E3633BAAC79493d908e63626716e204A45EdF); // LINK/USD on sepolia
+
+    // address private owner;   // if you do not want to display owner's address after deploy the contract
+
+    constructor() {}
+
+    function getLatestPrice() public view returns (uint256) {
+        (,int hopePrice,,,) = hopePriceFeed.latestRoundData();
+        (,int usdcPrice,,,) = usdcPriceFeed.latestRoundData();
+        (,int linkPrice,,,) = linkPriceFeed.latestRoundData();
+
+        uint256 hopePrice18Decimals = uint256(hopePrice) * 10**10;
+        uint256 usdcPrice18Decimals = uint256(usdcPrice) * 10**10;
+        uint256 linkPrice18Decimals = uint256(linkPrice) * 10**10;
+
+        uint256 totalHopePrice = uint256(hopePrice18Decimals) * hopeRatio;
+        uint256 totalUsdcPrice = uint256(usdcPrice18Decimals) * usdcRatio;
+        uint256 totalLinkPrice = uint256(linkPrice18Decimals) * linkRatio;
+
+        return (totalHopePrice + totalUsdcPrice + totalLinkPrice) / 100;
     }
+
     function transfer(address recipient, uint amount) external returns (bool) {
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
-
 
     // create the Portfolio tokens, and sent to msg.sender account
     function mint(uint amount) external {
@@ -62,9 +80,19 @@ contract ECR20_TestToken is IERC20 {
         emit Transfer(msg.sender, address(0), amount);
     }
 
-    function getBalanceOf() external view returns (uint) {
+    // get contract creator's balance
+    function getOwnerBalanceOf() external view returns (uint) {
         return balanceOf[msg.sender];
     }
+    
+    function getContractAddress() public view returns (address) {
+        return address(this);
+    }
+
+    function getTotalSupply() public view returns (uint){
+        return totalSupply;
+    }
+
 }
 
 
